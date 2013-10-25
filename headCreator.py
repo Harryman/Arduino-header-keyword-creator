77660ed5aa701d0b13a58e930be8baffdfd4b465
Harryman <uber1337newb@gmail.com>
2013-10-24 22:10:33 -0500

    Broke this out so I can link it to the sublime text plugin, just run

--- headCreator.py
+++ headCreator.py
@@ -28,142 +28,140 @@
 # include your YOURLIB.h before any other includes
 
 
-
 import os.path
-def sbtwn(str,idx,a,b):#this finds the instance of b first then rfinds a keep in mind
-	end = str.find(b,idx)
-	start = str.rfind(a,0,end)+len(a)
-	ret = str[start:end],start,end
-	return ret
 
-
-def getFunc(str,start):
-	if(str.find(libName+"::",start)>0):
-		funcType = sbtwn(str,start,'\n',libName+"::")
-		func = sbtwn(str,funcType[2],'::','{')
-		keyword = sbtwn(str,funcType[2],'::','(')
-		tpnt = str.find('}',func[2])
-
-		cmnts = ''
-		pabvcmnts = str.find('//',str.rfind('\n',0,funcType[1]-1),funcType[1])
-		if(pabvcmnts<0):
-			pabvcmnts = str.find('*/',str.rfind('\n',0,funcType[1]-1),funcType[1])
-			if(pabvcmnts > 0):
-				abvcmnts = str[str.rfind('/*',0,pabvcmnts):pabvcmnts+2]
-			else:
-				abvcmnts = str[pabvcmnts:str.find('\n', pabvcmnts)]
-			cmnts += abvcmnts 
-		pcmnts = str.find('//',func[2],str.find('\n',func[2]))
-		if(pcmnts>0):
-			inlinecmnts = sbtwn(str, pcmnts, '{', '\n')[0]
-			cmnts += inlinecmnts
-		ret = funcType[0]+func[0],keyword[0],tpnt,cmnts
-		return ret
-	else:
-		ret = '','',-1
+class headCreator():
+	def sbtwn(str,idx,a,b):#this finds the instance of b first then rfinds a keep in mind
+		end = str.find(b,idx)
+		start = str.rfind(a,0,end)+len(a)
+		ret = str[start:end],start,end
 		return ret
 
-path = raw_input('file:')
-while(os.path.isfile(path)==False):
-	path = raw_input('try again:')
-endPoint = path.find('.cpp')
-bpnt = path.rfind('\\',0,endPoint)+1
-libName = path[bpnt:endPoint]
 
-cppfile = open(path,'r')
-cpp = cppfile.read()
-cppfile.close()	
+	def getFunc(str,start):
+		if(str.find(headCreator.libName+"::",start)>0):
+			funcType = headCreator.sbtwn(str,start,'\n', headCreator.libName+"::")
+			func = headCreator.sbtwn(str,funcType[2],'::','{')
+			keyword = headCreator.sbtwn(str,funcType[2],'::','(')
+			tpnt = str.find('}',func[2])
 
-if(os.path.isfile(path[0:bpnt]+libName+'.h')):
-	ohfilef = open(path[0:bpnt]+libName+'.h')
-	ohfile = ohfilef.read()
-	ohfilef.close()
-	tmp = ''
-	tpnt = 0
-	while(ohfile.find('/*',tpnt)>0):### Strips out all of the comments in header, simpler to match
-		tmp += ohfile[tpnt:ohfile.find('/*',tpnt)]
-		tpnt = ohfile.find('*/',tpnt)+2
-	tmp += ohfile[tpnt:]
-	tpnt = 0
-	ohfile = ''
-	while(tmp.find('//',tpnt)>0):#remove single line comments next
-		ohfile += tmp[tpnt:tmp.find('//',tpnt)]
-		tpnt = tmp.find('//',tpnt)
-		tpnt = tmp.find('\n',tpnt)
-	ohfile += tmp[tpnt:]
-
-
-h = cpp[0:cpp.find('#')-1]
-h +='\n#ifndef '+libName+'_h\n'
-h += '#define '+libName+'_h\n\n'
-h += '#include "Arduino.h"\n\n'
-h += 'class '+libName+'{\n  '
-pubvar = ''
-pubfunc = 'public:\n'
-privar = ''
-prifunc = '\n  private:\n'
-exclude = []
-tpnt = cpp.find(libName+'::'+libName)
-ret = sbtwn(cpp,tpnt,'::','{')[0]+";\n"
-tpnt = tpnt + len(ret)
-pubfunc += '    '+ret
-txt = '##################################\n# Class Name\n##################################\n'+libName+'\tKEYWORD1\n\n##################################\n# Methods/Functions \n##################################\n'
-
-while(getFunc(cpp,tpnt)[2]>0):#gets functions
-	tstr = getFunc(cpp,tpnt)
-	if(tstr[0][0] !='_'):
-		pubfunc += '    '+tstr[0]+';'+tstr[3]+'\n'
-		txt = txt+tstr[1]+'\tKEYWORD2\n'
-	else:
-		prifunc += '    '+tstr[0]+';'+tstr[3]+'\n'
-	func = sbtwn(tstr[0],0,'(',')')[0]+',' #gets arguments adds comma for easy looping
-	tidx = 0
-	while((sbtwn(func,tidx,' ',',')[2])>0): # goes through func args adding them to exclude
-		args = sbtwn(func,tidx,' ',',')
-		tidx = args[2]+1
-		exclude += [args[0]]
-	tpnt = tstr[2]
-tpnt = 0
-
-
-while(cpp.find(' = ',tpnt)>0):#selects anything left of an assignment will not dupe
-	var = sbtwn(cpp, tpnt, ' ', ' = ')
-	if(len(var[0])>len(sbtwn(cpp,tpnt, '\t', ' = ')[0])):
-		var = sbtwn(cpp, tpnt, '\t', ' = ')
-	tpnt = var[2]+1
-	cnt = 0
-	for a in exclude:
-		if(var[0] == a):
-			cnt += 1
-	if(cnt == 0):
-		exclude += [var[0]]
-		try:
-			tp = ohfile.find(var[0])
-			if(tp > 0):
-				t = sbtwn(ohfile,tp-1,' ', ' ')
-				if(len(t[0])>len(sbtwn(ohfile,tp-1,'\t',' ')[0])):
-					t = sbtwn(ohfile,tp-1,'\t',' ')
-				if(var[0][0] != '_'):
-					pubvar += '    '+t[0]+' '+var[0]+';\n'
+			cmnts = ''
+			pabvcmnts = str.find('//',str.rfind('\n',0,funcType[1]-1),funcType[1])
+			if(pabvcmnts<0):
+				pabvcmnts = str.find('*/',str.rfind('\n',0,funcType[1]-1),funcType[1])
+				if(pabvcmnts > 0):
+					abvcmnts = str[str.rfind('/*',0,pabvcmnts):pabvcmnts+2]
 				else:
-					privar += '    '+t[0]+' '+var[0]+';\n'
-			else:
-				if(var[0][0] != '_'):
-					pubvar += '    //????'+var[0]+';\n' # if no var exsists you will have to set manually
-				else:
-					privar += '    //????'+var[0]+';\n'
-		except NameError:
-			if(var[0][0] != '_'):
-				pubvar += '    //????'+var[0]+';\n' # if no file exsists you will have to set manually
-			else:
-				privar += '    //????'+var[0]+';\n'
+					abvcmnts = str[pabvcmnts:str.find('\n', pabvcmnts)]
+				cmnts += abvcmnts 
+			pcmnts = str.find('//',func[2],str.find('\n',func[2]))
+			if(pcmnts>0):
+				inlinecmnts = headCreator.sbtwn(str, pcmnts, '{', '\n')[0]
+				cmnts += inlinecmnts
+			ret = funcType[0]+func[0],keyword[0],tpnt,cmnts
+			return ret
+		else:
+			ret = '','',-1
+			return ret
 
-h += pubfunc +'\n'+ pubvar + prifunc +'\n'+ privar+'\n};\n\n#endif'
 
-hfile = open(path[0:bpnt]+libName+'.h','w')
-hfile.write('// file generated using https://github.com/Harryman/Arduino-header-keyword-creator\n\n'+h)
-hfile.close()
-txtfile = open(path[0:bpnt]+'keywords.txt','w')
-txtfile.write('# file generated using https://github.com/Harryman/Arduino-header-keyword-creator\n'+txt)
-txtfile.close()
-raw_input('done! press enter to exit')
+	def createFiles(path):
+		endPoint = path.find('.cpp')
+		bpnt = path.rfind('\\',0,endPoint)+1
+		libName = path[bpnt:endPoint]
+		cppfile = open(path,'r')
+		cpp = cppfile.read()
+		cppfile.close()	
+
+		if(os.path.isfile(path[0:bpnt]+libName+'.h')):
+			ohfilef = open(path[0:bpnt]+libName+'.h')
+			ohfile = ohfilef.read()
+			ohfilef.close()
+			tmp = ''
+			tpnt = 0
+			while(ohfile.find('/*',tpnt)>0):### Strips out all of the comments in header, simpler to match
+				tmp += ohfile[tpnt:ohfile.find('/*',tpnt)]
+				tpnt = ohfile.find('*/',tpnt)+2
+			tmp += ohfile[tpnt:]
+			tpnt = 0
+			ohfile = ''
+			while(tmp.find('//',tpnt)>0):#remove single line comments next
+				ohfile += tmp[tpnt:tmp.find('//',tpnt)]
+				tpnt = tmp.find('//',tpnt)
+				tpnt = tmp.find('\n',tpnt)
+			ohfile += tmp[tpnt:]
+
+
+		h = cpp[0:cpp.find('#')-1]
+		h +='\n#ifndef '+libName+'_h\n'
+		h += '#define '+libName+'_h\n\n'
+		h += '#include "Arduino.h"\n\n'
+		h += 'class '+libName+'{\n  '
+		pubvar = ''
+		pubfunc = 'public:\n'
+		privar = ''
+		prifunc = '\n  private:\n'
+		exclude = []
+		tpnt = cpp.find(libName+'::'+libName)
+		ret = headCreator.sbtwn(cpp,tpnt,'::','{')[0]+";\n"
+		tpnt = tpnt + len(ret)
+		pubfunc += '    '+ret
+		txt = '##################################\n# Class Name\n##################################\n'+libName+'\tKEYWORD1\n\n##################################\n# Methods/Functions \n##################################\n'
+
+		while(headCreator.getFunc(cpp,tpnt)[2]>0):#gets functions
+			tstr = headCreator.getFunc(cpp,tpnt)
+			if(tstr[0][0] !='_'):
+				pubfunc += '    '+tstr[0]+';'+tstr[3]+'\n'
+				txt = txt+tstr[1]+'\tKEYWORD2\n'
+			else:
+				prifunc += '    '+tstr[0]+';'+tstr[3]+'\n'
+			func = headCreator.sbtwn(tstr[0],0,'(',')')[0]+',' #gets arguments adds comma for easy looping
+			tidx = 0
+			while((headCreator.sbtwn(func,tidx,' ',',')[2])>0): # goes through func args adding them to exclude
+				args = headCreator.sbtwn(func,tidx,' ',',')
+				tidx = args[2]+1
+				exclude += [args[0]]
+			tpnt = tstr[2]
+		tpnt = 0
+
+
+		while(cpp.find(' = ',tpnt)>0):#selects anything left of an assignment will not dupe
+			var = headCreator.sbtwn(cpp, tpnt, ' ', ' = ')
+			if(len(var[0])>len(headCreator.sbtwn(cpp,tpnt, '\t', ' = ')[0])):
+				var = headCreator.sbtwn(cpp, tpnt, '\t', ' = ')
+			tpnt = var[2]+1
+			cnt = 0
+			for a in exclude:
+				if(var[0] == a):
+					cnt += 1
+			if(cnt == 0):
+				exclude += [var[0]]
+				try:
+					tp = ohfile.find(var[0])
+					if(tp > 0):
+						t = headCreator.sbtwn(ohfile,tp-1,' ', ' ')
+						if(len(t[0])>len(sbtwn(ohfile,tp-1,'\t',' ')[0])):
+							t = headCreator.sbtwn(ohfile,tp-1,'\t',' ')
+						if(var[0][0] != '_'):
+							pubvar += '    '+t[0]+' '+var[0]+';\n'
+						else:
+							privar += '    '+t[0]+' '+var[0]+';\n'
+					else:
+						if(var[0][0] != '_'):
+							pubvar += '    //????'+var[0]+';\n' # if no var exsists you will have to set manually
+						else:
+							privar += '    //????'+var[0]+';\n'
+				except NameError:
+					if(var[0][0] != '_'):
+						pubvar += '    //????'+var[0]+';\n' # if no file exsists you will have to set manually
+					else:
+						privar += '    //????'+var[0]+';\n'
+
+		h += pubfunc +'\n'+ pubvar + prifunc +'\n'+ privar+'\n};\n\n#endif'
+
+		hfile = open(path[0:bpnt]+headCreator.libName+'.h','w')
+		hfile.write('// file generated using https://github.com/Harryman/Arduino-header-keyword-creator\n\n'+h)
+		hfile.close()
+		txtfile = open(path[0:bpnt]+'keywords.txt','w')
+		txtfile.write('# file generated using https://github.com/Harryman/Arduino-header-keyword-creator\n'+txt)
+		txtfile.close()
